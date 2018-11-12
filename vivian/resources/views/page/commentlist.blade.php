ef98b7b0b18772046ccd8d157e62161e0f91a4cb
Thien-Nguyen-Duc-23 <tducnguyen1997@gmail.com>
2018-11-12 13:19:21 +0700

    create master

---
+++ vivian/resources/views/page/commentlist.blade.php
@@ -0,0 +1,6 @@
+@foreach($testcmt as $comment)
+                        <div >
+                          <b>ten</b>: {{ $comment->id }}
+                          <p style="font-size:9px;">date</p>
+                        </div>
+                      @endforeach
